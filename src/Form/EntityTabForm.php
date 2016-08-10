cda2ccbaedf75643116038517a86417e4f9c75a2
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-08-10 22:43:41 +0100

    add form works!

--- src/Form/EntityTabForm.php
+++ src/Form/EntityTabForm.php
@@ -11,12 +11,54 @@ use Drupal\Core\Form\FormStateInterface;
 class EntityTabForm extends EntityForm {
 
   /**
+   * The entity type for which the entity tab is being created.
+   *
+   * @var string
+   */
+   // ADD FORM ONLY TODO!
+  protected $targetEntityTypeId;
+
+  // entity_type.bundle.info
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function init(FormStateInterface $form_state) {
+    parent::init($form_state);
+    $this->entityType = $this->entityManager->getDefinition($this->entity->getEntityTypeId());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $entity_type_id = NULL) {
+    // ADD FORM ONLY TODO!
+
+    $this->targetEntityTypeId = $entity_type_id;
+    $form = parent::buildForm($form, $form_state);
+    // Change replace_pattern to avoid undesired dots.
+    //$form['id']['#machine_name']['replace_pattern'] = '[^a-z0-9_]+';
+    $definition = $this->entityManager->getDefinition($this->targetEntityTypeId);
+    $form['#title'] = $this->t('Add new %label @entity-type', array(
+      '%label' => $definition->getLabel(),
+      '@entity-type' => $this->entityType->getLowercaseLabel(),
+    ));
+    return $form;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
 
     $entity_tab = $this->entity;
+    dsm($entity_tab);
+    
+    // Add only:
+    $entity_tab->targetEntityType = $this->targetEntityTypeId;
+    
+    
     $form['label'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Label'),
@@ -35,7 +77,26 @@ class EntityTabForm extends EntityForm {
       '#disabled' => !$entity_tab->isNew(),
     ];
 
-    /* You will need additional form elements for your custom properties. */
+    // TODO: inject.
+    $entity_bundle_info = \Drupal::service('entity_type.bundle.info');
+    $bundles = $entity_bundle_info->getBundleInfo($this->targetEntityTypeId);
+    $bundle_options = [];
+    foreach ($bundles as $bundle_name => $bundle_info) {
+      $bundle_options[$bundle_name] = $bundle_info['label'];
+    }
+    natsort($bundle_options);
+
+    $form['target_bundles'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Bundles'),
+      '#description' => $this->t("The bundles of the target entity type on which this tab appears. Leave empty to show on all bundles."),
+      '#options' => $bundle_options,
+      '#default_value' => (array) $entity_tab->getTargetBundles(),
+    ];
+
+    // TODO:
+    // verb
+    // weight
 
     return $form;
   }
@@ -48,6 +109,7 @@ class EntityTabForm extends EntityForm {
 
     // Sets the entity type ID into the config name.
     // TODO: this only needs to happen when adding a new entity.
+    // ARGH where does this come from???????
     $form_state->setValueForElement($form['id'], $this->targetEntityTypeId . '.' . $form_state->getValue('id'));
   }
 
