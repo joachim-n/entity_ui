925f259810e6b039314e0b80684e01800d3df1d1
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-08-14 14:25:36 +0100

    plugin type

---
+++ src/Plugin/EntityTabContentBase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\entity_ui\Plugin;
+
+use Drupal\Component\Plugin\PluginBase;
+
+/**
+ * Base class for Entity tab content plugins.
+ */
+abstract class EntityTabContentBase extends PluginBase implements EntityTabContentInterface {
+
+
+  // Add common methods and abstract methods for your plugin type here.
+
+}
---
+++ src/Plugin/EntityTabContentInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\entity_ui\Plugin;
+
+use Drupal\Component\Plugin\PluginInspectionInterface;
+
+/**
+ * Defines an interface for Entity tab content plugins.
+ */
+interface EntityTabContentInterface extends PluginInspectionInterface {
+
+
+  // Add get/set methods for your plugin type here.
+
+}
