84e749802a4d7e96fdf82a587a94fa12501f5f53
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-08-14 14:39:02 +0100

    canonical plugin

---
+++ src/Plugin/EntityTabContent/Canonical.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\entity_ui\Plugin\EntityTabContent;
+
+use Drupal\entity_ui\Plugin\EntityTabContentBase;
+use Drupal\entity_ui\Plugin\EntityTabContentInterface;
+
+/**
+ * @EntityTabContent(
+ *   id = "canonical",
+ *   label = @Translation("Canonical entity tab"),
+ * )
+ */
+class Canonical {
+
+}
