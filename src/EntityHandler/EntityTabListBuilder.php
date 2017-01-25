e374d006183594f7d3bfaee90378f273380cb6e1
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-01-25 16:10:48 +0000

    WIP... more on list builder

--- src/EntityHandler/EntityTabListBuilder.php
+++ src/EntityHandler/EntityTabListBuilder.php
@@ -6,18 +6,88 @@ use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a listing of Entity tab entities. TODO
+ * Provides a listing of Entity tab entities for a single target entity type.
  */
 class EntityTabListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * The currently active route match object.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $currentRouteMatch;
+
+  /**
+   * The target entity type ID.
+   */
+  protected $target_entity_type_id;
+
+  /**
+   * Constructs a new EntityTabListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $current_route_match
+   *   The currently active route match object.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, RouteMatchInterface $current_route_match) {
+    parent::__construct($entity_type, $storage);
+
+    $this->currentRouteMatch = $current_route_match;
+
+    $this->target_entity_type_id = $current_route_match->getRouteObject()->getOption('_target_entity_type_id');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEntityIds() {
+    // No need to sort; load() does that.
+    $query = $this->storage->getQuery();
+    $query->condition('targetEntityType', $this->target_entity_type_id);
+
+    return $query->execute();
+  }
 
   /**
    * {@inheritdoc}
    */
   public function render() {
+    $build = parent::render();
+
+    // Tweak the empty text.
+    $build['table']['#empty'] = $this->t('There is no @label for @target_type_label yet.', [
+      '@label' => $this->entityType->getLabel(),
+      '@target_type_label' => $this->target_entity_type_id,
+    ]);
+
+    return $build;
+
+    /////////////
+    dsm($this->storage);
+    $route_match = \Drupal::service('current_route_match');
+    $route = $route_match->getRouteObject();
+
+    $target_entity_type = $route->getOption('_target_entity_type_id');
+    dsm($target_entity_type);
     dsm("list builder!");
     return [];
   }
