5e1493c3850d2939c516001877ef62b25c3798de
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-01-26 23:40:22 +0000

    Added to plugin interface

--- src/Plugin/EntityTabContentInterface.php
+++ src/Plugin/EntityTabContentInterface.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\entity_ui\Plugin;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Component\Plugin\PluginInspectionInterface;
 
 /**
@@ -9,7 +10,17 @@ use Drupal\Component\Plugin\PluginInspectionInterface;
  */
 interface EntityTabContentInterface extends PluginInspectionInterface {
 
-
-  // Add get/set methods for your plugin type here.
+  /**
+   * Provides a form array for the TODO plugin's settings form.
+   *
+   * @param array $form
+   *   The form array.
+   * @param FormStateInterface $form_state
+   *   The form state.
+   *
+   * @return array
+   *   The modified form array.
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state);
 
 }
