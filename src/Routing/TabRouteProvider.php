c6036777283f842376f707469fc87ea46426aba5
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-01-31 00:12:18 +0000

    Changed to new dummy function

--- src/Routing/TabRouteProvider.php
+++ src/Routing/TabRouteProvider.php
@@ -22,9 +22,13 @@ class TabRouteProvider implements EntityRouteProviderInterface {
    */
   public function getRoutes(EntityTypeInterface $entity_type) {
     $collection = new RouteCollection();
-    
-    foreach (entity_ui_get_ops() as $operation) {
-      $path = $entity_type->getLinkTemplate($operation);
+
+    foreach (entity_ui_get_tabs($entity_type) as $tab_id => $entity_tab) {
+      $path_component = $entity_tab->getPathComponent();
+
+      // $path = $entity_type->getLinkTemplate($tab_id);
+      $path = $entity_type->getLinkTemplate('canonical') . '/' . $path_component;
+
       $route = new Route($path);
       $route
         ->setDefaults([
@@ -37,10 +41,10 @@ class TabRouteProvider implements EntityRouteProviderInterface {
         ]);
       // TODO: requirements, options.
       $entity_type_id = $entity_type->id();
-      $collection->add("entity.{$entity_type_id}.{$operation}", $route);
-      //print_r("entity.{$entity_type_id}.{$operation}\n");
+      // TODO: namespace with a prefix?
+      $collection->add("entity.{$entity_type_id}.{$path_component}", $route);
     }
-    
+
     /*
     // TODO: need link template first!
     //$path = $entity_type->getLinkTemplate('moderation-form');
