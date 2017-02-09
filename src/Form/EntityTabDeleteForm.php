ea46d263162a46309f730baec5d0d11eedbc9de1
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-02-09 22:51:52 +0000

    tweaked delete message

--- src/Form/EntityTabDeleteForm.php
+++ src/Form/EntityTabDeleteForm.php
@@ -94,14 +94,9 @@ class EntityTabDeleteForm extends EntityConfirmFormBase {
     $this->routerBuilder->setRebuildNeeded();
     $this->menuLocalTaskPluginManager->clearCachedDefinitions();
 
-    drupal_set_message(
-      $this->t('content @type: deleted @label.',
-        [
-          '@type' => $this->entity->bundle(),
-          '@label' => $this->entity->label(),
-        ]
-        )
-    );
+    drupal_set_message($this->t('Deleted @label.', [
+      '@label' => $this->entity->label(),
+    ]));
 
     $form_state->setRedirectUrl($this->getCancelUrl());
   }
