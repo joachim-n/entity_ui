ca5913c4f65d8ebfbc02310579616129c967ffe0
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-02-09 23:05:14 +0000

    docs

--- src/EntityHandler/EntityUIAdminInterface.php
+++ src/EntityHandler/EntityUIAdminInterface.php
@@ -8,17 +8,33 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Defines the interface for Entity UI admin handlers.
  *
- *
+ * An Entity UI admin handler creates the admin UI for the collection of
+ * Entity Tabs of the target entity type. This allows each different target
+ * entity type to have its own admin UI for its Entity Tabs, which is added to
+ * its core admin UI in a way that's suitable for the menu and tab structure.
+ * For example, node Entity Tabs are added as a tab alongside the list of node
+ * types, whereas user Entity Tabs are added alongside the user field admin tabs
+ * as there are no user types.
  */
 interface EntityUIAdminInterface {
 
   /**
+   * Gets the routes for the entity type.
    *
+   * @return array
+   *  An array of route objects.
    */
-  // CAlled from subscriber
   public function getRoutes();
 
-  // called from derivative
+  /**
+   * Gets local task plugin derivatives.
+   *
+   * @param array $base_plugin_definition
+   *   The definition array of the base plugin.
+   *
+   * @return
+   *  An array of plugin derivatives for the local task for the entity type.
+   */
   public function getLocalTasks($base_plugin_definition);
 
   /**
@@ -32,7 +48,7 @@ interface EntityUIAdminInterface {
   public function localTasksAlter(&$local_tasks);
 
   /**
-   * Gets the definition of all derivatives of a base plugin. TODO
+   * Gets local action plugin derivatives.
    *
    * @param array $base_plugin_definition
    *   The definition array of the base plugin.
