feae6504cc69db2978ac1bd9541a3e15b5e4b54f
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-02-09 16:42:23 +0000

    expect plugins to get the entity tab.

--- src/Plugin/EntityTabContentManager.php
+++ src/Plugin/EntityTabContentManager.php
@@ -5,6 +5,7 @@ namespace Drupal\entity_ui\Plugin;
 use Drupal\Core\Plugin\DefaultPluginManager;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\entity_ui\Entity\EntityTabInterface;
 
 /**
  * Manages Entity tab content plugins.
@@ -29,6 +30,21 @@ class EntityTabContentManager extends DefaultPluginManager {
 
     $this->alterInfo('entity_ui_entity_tab_content_info');
     $this->setCacheBackend($cache_backend, 'entity_ui_entity_tab_content_plugins');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createInstance($plugin_id, array $configuration = array()) {
+    // Sanity check: we expect the Entity Tab entity this plugin is on to be in
+    // the configuration array.
+    // TODO: ask someone with more understanding of the innards of plugins about
+    // whether this is wrong.
+    if (!isset($configuration['entity_tab']) || !($configuration['entity_tab'] instanceof EntityTabInterface)) {
+      throw new \Exception("Entity tab plugin configuration must contain an Entity Tab entity.");
+    }
+
+    return parent::createInstance($plugin_id, $configuration);
   }
 
 }
