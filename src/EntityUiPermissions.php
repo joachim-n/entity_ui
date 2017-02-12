1169145ee72e738d3a0ad3e371e1f37641ae66ef
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-02-12 08:15:24 +0000

    Added dynamic permissions.

---
+++ src/EntityUiPermissions.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\entity_ui;
+
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides dynamic permissions for Entity Tab admin pages.
+ */
+class EntityUiPermissions implements ContainerInjectionInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new EntityUiPermissions instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
+   *   The entity manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('entity_type.manager'));
+  }
+
+  /**
+   * Returns an array of Entity UI admin permissions.
+   *
+   * @return array
+   */
+  public function entityUiAdminPermissions() {
+    $permissions = [];
+
+    foreach ($this->entityTypeManager->getDefinitions() as $entity_type_id => $entity_type) {
+      if ($this->entityTypeManager->hasHandler($entity_type_id, 'entity_ui_admin')) {
+        // Create a permission for each entity type to manage the Entity Tabs.
+        $permissions['administer ' . $entity_type_id . ' entity tabs'] = [
+          'title' => $this->t('%entity_label: Administer entity tabs', [
+            '%entity_label' => $entity_type->getLabel(),
+          ]),
+          'restrict access' => TRUE,
+        ];
+      }
+    }
+
+    return $permissions;
+  }
+
+}
